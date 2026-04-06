bdb926b6c3cbfc71b79a930f355e2771741dcfff
AMIT PRAJAPATI <amitprajapati4169@gmail.com>
2026-04-06 16:09:29 +0530

    Update vite.config.js with base config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'classic', // force JSX parsing in .js files
-      babel: {
-        presets: ['@babel/preset-react']
-      }
-    })
-  ],
+  base: '/',
+  plugins: [react()]
 })
